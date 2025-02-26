3988dafabcda784ed5f48ea46f50a3cba741c132
francomac <franco.mcarballo@outlook.com>
2025-02-25 22:08:55 -0600

    test for command script

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,45 @@
+"""
+test custom Django management commands
+"""
+
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """
+    Test custom Django management commands
+    """
+
+
+    def test_wait_for_db_ready(self, patched_check):
+        """
+        Test waiting for the database when the database is available
+        """
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(database=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """
+        Test waiting for the database when the database is not available
+        """
+        
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+        """the first 2 times mock method is called, it raises Psycopg2Error, \
+        the next 3 times it raises OperationalError, and the last time it returns True"""
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+
+        patched_check.assert_called_with(database=['default'])

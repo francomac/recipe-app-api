382ac0c9b5c9d8bf76e4f59d141501b77d651695
Franco Martinez <franco.martinez-carballo@empassion.com>
2025-12-11 16:45:43 -0600

    configured docker compose and checks to wait for DB

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,17 +6,18 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
-    """ Test the calc module """
+    """Test the calc module"""
 
     def test_add_numbers(self):
-        """ Test that two numbers are added together """
+        """Test that two numbers are added together"""
         res = calc.add(3, 8)
 
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        """ Test that values are subtracted and returned """
+        """Test that values are subtracted and returned"""
         res = calc.subtract(5, 11)
 
         self.assertEqual(res, 6)
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,12 +3,27 @@ Django Command to Wait for Database to be Available
 """
 
 from django.core.management.base import BaseCommand
+from psycopg2 import OperationalError as Psycopg2OpError
+from django.db.utils import OperationalError
+
+import time
 
 
 class Command(BaseCommand):
-    """ Django command to pause execution until database is available """
+    """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
-        self.stdout.write('Waiting for database...')
-        self.stdout.write('Database available!')
+        self.stdout.write("Waiting for database...")
+        self.stdout.write("Database available!")
+        db_up = False
+
+        while db_up is False:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
         pass
